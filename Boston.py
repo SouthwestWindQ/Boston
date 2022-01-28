6bce8ef725acef69b5796e6aed1c44928f58c1c9
qianweinan <ypqwn@stu.pku.edu.cn>
2022-01-28 23:46:22 +0800

    更正程序，让预测结果较为正常

--- Boston.py
+++ Boston.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch.nn as nn
 import pandas as pd
 from sklearn import datasets
+from sklearn.preprocessing import MinMaxScaler
 from matplotlib import pyplot as plt
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 
@@ -11,58 +12,63 @@ class MyModel(nn.Module):
     def __init__(self):
         super(MyModel,self).__init__()
         self.net = nn.Sequential(
-            nn.Linear(13,32),
+            nn.Linear(13,14),
             nn.ReLU(),
-            nn.Linear(32,1),
+            nn.Linear(14,1)
         )
     def forward(self,x):
         return self.net(x)
     
 table_x,table_y = datasets.load_boston(return_X_y=True)
-tensor_x = torch.from_numpy(table_x)
-tensor_y = torch.from_numpy(table_y).unsqueeze(1)
-myDataset = TensorDataset(tensor_x, tensor_y)
-tr_data = DataLoader(dataset=myDataset, batch_size=22, shuffle=True)
+scaler = MinMaxScaler()
+tensor_x = torch.from_numpy(scaler.fit_transform(table_x))
+tensor_y = torch.from_numpy(scaler.fit_transform(table_y[:,np.newaxis]))
+myTrainDataset = TensorDataset(tensor_x[0:350, : ], tensor_y[0:350, : ])
+myTestDataset = TensorDataset(tensor_x[351:506, : ], tensor_y[351:506, : ])
+tr_data = DataLoader(dataset=myTrainDataset, batch_size=22, shuffle=True)
+tt_data = DataLoader(dataset=myTestDataset, shuffle=False)
 
 model = MyModel().to('cpu')
 criterion = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+optimizer = torch.optim.RMSprop(model.parameters(), lr=1e-2)
 model.net = model.net.double()
 
-for epoch in range(100):
+iter_loss = []
+batch_loss = []
+for epoch in range(500):
     model.train()
     for x,y in tr_data:
         optimizer.zero_grad()
         x,y = x.to('cpu'),y.to('cpu')
         pred = model(x)
         loss = criterion(pred, y)
+        batch_loss.append(loss.data.numpy())
         loss.backward()
         optimizer.step()
+    iter_loss.append(np.average(np.array(batch_loss)))
+
+x = np.arange(500)
+y = np.array(iter_loss)
+plt.scatter(x,y)
+plt.show()
         
 model.eval()
 total_loss = 0 
-tt_data = DataLoader(dataset=myDataset, shuffle=False)
 for x,y in tt_data:
     x,y = x.to('cpu'), y.to('cpu')
     with torch.no_grad():  #禁止计算梯度，只计算output(速度加快)
         pred = model(x)
         # print(x)
-        print(pred)
-        # print(y)
+        print(pred, y)
         loss = criterion(pred, y)
         total_loss += loss.cpu().item()*len(x)
         
 avg_loss = total_loss / len(tt_data.dataset)
 print(avg_loss)
-cnt = 0
-for paras in model.parameters():
-    cnt += 1
-    if cnt==4:
-        print(paras)
 
 # #x1 = np.array(table['B'])
 # x2 = np.array(table['LSTAT'])
 # y = np.array(table['MEDV'])
 # #plt.scatter(x1,y)
 # plt.scatter(x2,y)
-# plt.show()
+# plt.show()'''
