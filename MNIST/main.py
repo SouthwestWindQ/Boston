2d6d5868f1851b84cf2ac13cbcfdab0028e24ccd
qianweinan <ypqwn@stu.pku.edu.cn>
2022-01-31 16:03:59 +0800

    delete some explanatory notes

--- MNIST/main.py
+++ MNIST/main.py
@@ -22,10 +22,6 @@ class MyModel(nn.Module):
             nn.Softmax(dim=1)
         )
     def forward(self,x):
-        # x = fun.relu(self.net1(x))
-        # x = fun.dropout(x, p=0.5)
-        # x = fun.relu(self.net2(x))
-        # x = fun.dropout(x, p=0.5)
         return self.net(x.view(-1,784))
         
 tr_data = DataLoader(
@@ -44,15 +40,6 @@ tt_data = DataLoader(
         ])),
     batch_size=64, shuffle=True,
 )
-# table_x,table_y = fetch_california_housing(return_X_y=True)
-# scaler = MinMaxScaler()
-# tensor_x = torch.from_numpy(scaler.fit_transform(table_x))
-# tensor_y = torch.from_numpy(table_y[:,np.newaxis])
-# # tensor_y = torch.from_numpy(scaler.fit_transform(table_y[:,np.newaxis]))
-# myTrainDataset = TensorDataset(tensor_x[0:14450, : ], tensor_y[0:14450, : ])
-# myTestDataset = TensorDataset(tensor_x[14451:20640, : ], tensor_y[14451:20640, : ])
-# tr_data = DataLoader(dataset=myTrainDataset, batch_size=22, shuffle=True)
-# tt_data = DataLoader(dataset=myTestDataset, shuffle=False)
 
 iter_loss = []
 batch_loss = []
@@ -79,12 +66,6 @@ for epoch in range(epoN):
         loss.backward()
         optimizer.step()
     iter_loss.append(np.average(np.array(batch_loss)))
-
-    # x = np.arange(epoN)
-    # y = np.array(iter_loss)
-    # plt.scatter(x,y)
-    # plt.show()
-    # print(iter_loss[epoN-1])
         
     model.eval()
     total_loss = 0 
@@ -103,8 +84,6 @@ for epoch in range(epoN):
                 tot += 1
                 if max1==y[i]:
                     cor += 1
-            # print(x)
-            # print(scaler.inverse_transform(pred), scaler.inverse_transform(y))
             loss = criterion(pred, y)
             total_loss += loss.cpu().item()*len(x)
             
@@ -129,38 +108,3 @@ y2 = np.array(avg_list)
 plt.scatter(x,y1)
 plt.scatter(x,y2)
 plt.show()
-        
-# z = np.array(avg_list)
-# x_list, y_list = [], []
-# for i in range(1,31):
-#     for j in range(250):
-#         x_list.append(i)
-#         y_list.append(j)
-# x,y = np.array(x_list),np.array(y_list)
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-# ax.scatter(x, y, z, cmap=cm.jet, linewidth=0.01)
-# plt.show()
-
-# #x1 = np.array(table['B'])
-# x2 = np.array(table['LSTAT'])
-# y = np.array(table['MEDV'])
-# #plt.scatter(x1,y)
-# plt.scatter(x2,y)
-# plt.show()'''
-
-# delta = 0.1
-# x = np.array([1,2,3,4])
-# y = np.array([1,2,3,4])
-# # X, Y = np.meshgrid(x, y)
-# z = np.array([])
-# # x=X.flatten()
-# # y=Y.flatten()
-# # z=Z.flatten()
- 
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-# ax.scatter(x, y, z, cmap=cm.jet, linewidth=0.01)
-# plt.show()
-# plt.contour(X,Y,Z)
-# plt.show()
