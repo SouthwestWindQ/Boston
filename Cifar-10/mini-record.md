c1cd7d77fedb290e05316fe8b4ea3387b117e8a7
qianweinan <ypqwn@stu.pku.edu.cn>
2023-01-18 21:02:28 +0800

    comments

--- Cifar-10/mini-record.md
+++ Cifar-10/mini-record.md
@@ -10,11 +10,11 @@
 
 然后，我根据论文进行了一定调整，首先超参完全与论文一致，设置`batch_size=128`，优化器采用SGD（初始 `lr=0.1`，`weight_decay=1e-4`，`momentum=0.9`），学习率仅在经过32k与48k步之后除以10（这点没有与论文完全一致），然后还进行了随机裁剪与随机水平翻转的数据增强，normalization也改成了三维，没有进行weight initialization。跑200个epoch，最后的结果比较意外：训练集和测试集的acc都收敛在**10%**，loss集中在2.3左右且无法下降，说明模型未收敛，但原因不明。
 
-一开始我认为可能是初始 `lr` 过大，因此将初始的 `lr` 直接修改为1e-3，最终acc成功收敛到88.3%。但将model直接换为resnet18（`from torchvision.models.resnet import resnet18`）后发现即使初始 `lr` 为0.1，模型可以成功收敛到acc 大于10%，因此猜测是模型的问题。仔细审查论文之后，发现我没有做**weight initialization**的工作，查阅资料后发现原论文使用的weight initialization的方法是Kaiming He采用的方法，将其运用于所有卷积层与全连接层后（本来最后一层之前的avgpool直接采用的是 `torch.nn.functional` 中的 `AvgPool2d`，这一版将其加入了网络之中，使用的是 `torch.nn.AdaptiveAvgPool2d`，但这一改动貌似关系不大），测试集acc成功收敛到91.3%，说明<u>weight initialization可能会对实验的结果产生显著的影响</u>。
+一开始我认为可能是初始 `lr` 过大，因此将初始的 `lr` 直接修改为1e-3，最终acc成功收敛到88.3%。但将model直接换为resnet18（`from torchvision.models.resnet import resnet18`）后发现即使初始 `lr` 为0.1，模型可以成功收敛到acc大于10%，因此猜测是模型的问题。仔细审查论文之后，发现我没有做**weight initialization**的工作，查阅资料后发现原论文使用的weight initialization的方法是Kaiming He采用的方法，将其运用于所有卷积层与全连接层后（本来最后一层之前的avgpool直接采用的是 `torch.nn.functional` 中的 `AvgPool2d`，这一版将其加入了网络之中，使用的是 `torch.nn.AdaptiveAvgPool2d`，但这一改动貌似关系不大），测试集acc成功收敛到91.3%，说明<u>weight initialization可能会对实验的结果产生显著的影响</u>。
 
 > 比较有趣的是，我在网上也查到pytorch的神经网络默认采用kaiming初始化，不过我把初始化前后的网络层参数进行输出，发现参数还是存在不同，说明我可能采用了二次kaiming初始化的策略，不知道为什么这对结果是否收敛产生了巨大的影响。
 
-再次与论文对比，发现自己对 `lr` 的处理策略还存在一定问题，原论文在一开始先采用了1e-2的学习率以warmup，待acc达到20%以上再调整为0.1，进而采用先前的递降策略。这或许可以解释之前在未进行合适的初始化时模型无法收敛的现象。采用这一warmup策略（节点设为1k次迭代），最终测试集acc成功收敛到93.9%（在120个epoch左右便收敛到这一值），大致完成了94%的目标。
+再次与论文对比，发现自己对 `lr` 的处理策略还存在一定问题，原论文在一开始先采用了1e-2的学习率以warmup，待acc达到20%以上再调整为0.1，进而采用先前的递降策略。这或许可以解释之前在未进行合适的初始化时模型无法收敛的现象。采用这一warmup策略（节点设为500次迭代），最终测试集acc成功收敛到93.9%（在120个epoch左右便收敛到这一值），大致完成了94%的目标。
 
 ![final result](result.png)
 
--- Cifar-10/resnet.py
+++ Cifar-10/resnet.py
@@ -15,23 +15,27 @@ class ResBlock(nn.Module):
         )
         nn.init.kaiming_normal_(
             self.conv1.weight, mode='fan_out', nonlinearity='relu'
-        )
+        )      # 对残差块的卷积层进行kaiming初始化
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.conv2 = nn.Conv2d(
             out_channels, out_channels, 3, stride=1, padding=1, bias=False
         )
         nn.init.kaiming_normal_(
             self.conv2.weight, mode='fan_out', nonlinearity='relu'
-        )
+        )      # 对残差块的卷积层进行kaiming初始化
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.subsample = subsample
     
     def forward(self, x):
+        '''
+        forward - 根据ResNet的定义实现残差块的前递函数, 返回残差块的output
+        '''
         id = x if self.subsample is None else self.subsample(x)
+               # 注意判断是否需要subsampling
         x = F.relu(self.bn1(self.conv1(x)))
         x = self.bn2(self.conv2(x))
         x += id
-        x = F.relu(x)
+        x = F.relu(x)  # ReLU需要加在x加上原来的值之后
         return x
 
 
@@ -43,7 +47,7 @@ class MyResNet(nn.Module):
         self.conv = nn.Conv2d(3, 16, 3, stride=1, padding=1, bias=False)
         nn.init.kaiming_normal_(
             self.conv.weight, mode='fan_out', nonlinearity='relu'
-        )
+        )      # 对第一层卷积层进行kaiming初始化
         self.bn = nn.BatchNorm2d(16)
         self.layer1 = self.make_layer(16, 1)
         self.layer2 = self.make_layer(32, 2)
@@ -52,40 +56,42 @@ class MyResNet(nn.Module):
         self.fc = nn.Linear(64, 10)
         nn.init.kaiming_normal_(
             self.fc.weight, mode='fan_out', nonlinearity='relu'
-        )
+        )      # 对全连接层进行kaiming初始化
     
     def make_layer(self, out_channels, stride):
         subsample = nn.Sequential(
             nn.Conv2d(self.in_channels, out_channels, 1, stride=2, bias=False),
             nn.BatchNorm2d(out_channels)
         ) if out_channels != self.in_channels else None
+               # 只有第2、3个layer需要subsampling
+               # 直接用kernel size=1, stride=2的卷积层来进行subsampling
         if subsample:
             nn.init.kaiming_normal_(
                 subsample[0].weight, mode='fan_out', nonlinearity='relu'
-            )
+            )  # 对卷积层进行kaiming初始化
         layers = [ResBlock(self.in_channels, out_channels, stride, subsample)]
+               # 首先加第一个残差块，可能包含subsampling，用于压缩大小
         self.in_channels = out_channels
-        for _ in range(1, self.N):
-            layers.append(ResBlock(out_channels, out_channels))
-        return nn.Sequential(*layers)
+        for _ in range(1, self.N):      # 加入普通的残差块
+            layers.append(ResBlock(out_channels, out_channels))  
+        return nn.Sequential(*layers)   # 这样做方便网络的加深 (只需要改变self.N)
     
     def forward(self, x):
-        # x = F.relu(self.bn(self.conv(x)))
-        x = self.conv(x)
+        x = self.conv(x)     # 这里直接经过卷积，不经过BN和ReLU，防止过拟合
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        x = self.avgpool(x)
+        x = self.avgpool(x)  # average pooling作为成员之一，或许可以参与训练？
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
 
-        
+
 train_loader = DataLoader(
     datasets.CIFAR10('data', train=True, download=True,
         transform=transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, padding=4),  # 数据增强 - 随机裁剪
+            transforms.RandomHorizontalFlip(),     # 数据增强 - 随机水平翻转
             transforms.ToTensor(),
             transforms.Normalize(
                 mean=(0.4914, 0.4822, 0.4465),
@@ -93,7 +99,7 @@ train_loader = DataLoader(
             ) # 注意input image是三通道的，所以mean和std需要三维
         ])),
     batch_size=128, shuffle=True, drop_last=False,
-)
+) # 这里训练数据集采用数据增强以增加数据量，同时防止过拟合，有助于提升测试集acc
 test_loader = DataLoader(
     datasets.CIFAR10('data', train=False, download=True, 
         transform=transforms.Compose([
@@ -116,16 +122,19 @@ model = MyResNet().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(
     model.parameters(), lr=0.1, weight_decay=1e-4, momentum=0.9
-)
-# 首先用0.01的lr进行warmup，正确率>20%时转为0.1，然后
-# 在iteration = 32k、48k时分别在原lr的基础上乘以0.1，逐步下降
+)  # 根据原论文，采用随机梯度下降法，超参数与原论文一致
+
+# scheduler主要实现的是原论文中lr变化的策略
+# 首先用0.01的lr进行warmup，当训练集acc>20% (这边设置为500次迭代) 时转为0.1，
+# 然后在iteration = 32k、48k时分别在原lr的基础上乘以0.1，使lr逐步下降
 scheduler = torch.optim.lr_scheduler.LambdaLR(
     optimizer, lr_lambda=lambda iter: 
         0.1 if iter < 500 else 
         1 if iter < 32000 else 
         0.1 if iter < 48000 else 0.01
-) 
+)  # 注意这里的lr_lambda的返回值是initial lr乘以的倍率，而非lr本身
 
+# 每个epoch都进行train与test的过程
 for epoch in range(epoN):
     model.train()
     progress_bar = tqdm(train_loader, total=len(train_loader)) 
@@ -151,7 +160,7 @@ for epoch in range(epoN):
     progress_bar = tqdm(test_loader, total =len(test_loader))
     for x, y in progress_bar:
         x, y = x.to(device), y.to(device)
-        with torch.no_grad():  #禁止计算梯度，只计算output(速度加快)
+        with torch.no_grad():  # 禁止计算梯度，只计算output (速度加快)
             pred = model(x)
             loss = criterion(pred, y)
             total_loss += loss.cpu().item()*len(x)
